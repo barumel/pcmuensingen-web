5a59bbfdfdf546ea15cb090200938e6be4a85f56
Manuel Kipfer <manuel.kipfer@bluewin.ch>
2021-04-24 19:41:05 +0200

    Add some styles

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import moment from 'moment';
 import 'moment/locale/fr-ch';
 import 'moment/locale/de-ch';
 
+import './bootstrap.css';
 import './App.css';
 import Layout from './containers/Layout';
 import Home from './containers/Home';
